325426cd1329ebbc4f2c9bad7250aedd4c2a9e33
Nik Afiq <nik.afiq98@ymail.com>
2022-06-08 04:09:56 +0900

    Main program created

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import re
+import chardet
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+#Tk().withdraw() #no full GUI
+#testfile = askopenfilename() #show an "Open" dialog box
+testfile = 'test.txt' #temporary filepath for testing purpose
+
+print("Your test file is: "+testfile)
+print(open(testfile, 'r').read)
+with open(testfile, 'rb') as f:
+    #print("The txt file contain: "+f.read(testfile))
+    print(chardet.detect(f.read()))
+    f.close()
+
+test_str = testfile
+with open(test_str, 'r') as file:
+    print(file)
